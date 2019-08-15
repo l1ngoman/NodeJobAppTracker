a8af0222bc12b76c5572efa6bcc751edfce3f888
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2019-08-15 13:12:01 -0700

    ATG:: Fixed bug with nodemon.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const index = require('../controllers/index.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index.get_index);
 
 module.exports = router;
