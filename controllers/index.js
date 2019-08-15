914666a2585aba4ec8c1319d205e6761acffef40
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2019-08-15 14:17:35 -0700

    ATG:: Added form with date. Added function to autopopulate today's date with a helper function.

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,13 @@
+const date = require('../helpers/dateFunctions.js');
+
 exports.get_index = function(req, res, next) {
- res.render('index', { title: 'Goodbye' });
+ res.render('index', {
+        title: 'Goodbye',
+        today: date.today()
+    });
+}
+
+exports.submit_form = function(req, res, next) {
+ console.log("Job Title: ", req.body.jobTitle);
+ res.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const index = require('../controllers/index.js');
 
 /* GET home page. */
 router.get('/', index.get_index);
+router.post('/', index.submit_form);
 
 module.exports = router;
